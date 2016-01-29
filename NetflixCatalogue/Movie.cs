b4e9b0d289b75db094176b34aa1b6c85b63723b6
yaelschlotman <koshercouponmom@gmail.com>
2016-01-29 11:00:00 -0600

    create Title class with overloaded constructors for name and rating.

---
+++ NetflixCatalogue/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixCatalogue
+{
+    /*class Movie : Title
+    {
+        private float duration; 
+        public Movie()
+         {
+
+        }
+    }*/
+}
---
+++ NetflixCatalogue/ViewGenresAndTitles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixCatalogue
+{
+    class ViewGenresAndTitles 
+    {
+        List<string> Genres = new List<string> { "Comedy", "Drama", "Action"}; 
+        public ViewGenresAndTitles()
+        {
+
+        }
+    }
+}
